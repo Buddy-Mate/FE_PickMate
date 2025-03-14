125957b85afc16fc9ec7b388993f168d4f0c5497
김보미 <kimbomi2172@naver.com>
2025-03-14 18:09:08 +0900

    ✨ feat: 스터디 페이지 기본 구현

--- src/pages/study/index.tsx
+++ src/pages/study/index.tsx
@@ -1,11 +1,15 @@
 import StudyBanner from '@/components/Banner/StudyBanner'
 import Dropdown from '@/components/Dropdown'
+import Pagination from '@/components/Pagination'
 import SearchBar from '@/components/SearchBar'
+import StudyList from '@/components/StudyList'
+import { STUDIES } from '@/constants/STUDIES'
+import { useState } from 'react'
 
 export default function Study() {
-  // const [currentPage, setCurrentPage] = useState(1)
-  // const itemsPerPage = 6
-  // const totalPages = Math.ceil(PROJECTS.length / itemsPerPage)
+  const [currentPage, setCurrentPage] = useState(1)
+  const itemsPerPage = 6
+  const totalPages = Math.ceil(STUDIES.length / itemsPerPage)
 
   return (
     <div>
@@ -15,12 +19,12 @@ export default function Study() {
           <Dropdown />
           <SearchBar />
         </div>
-        {/* <ProjectList currentPage={currentPage} /> */}
-        {/* <Pagination
+        <StudyList currentPage={currentPage} />
+        <Pagination
           currentPage={currentPage}
           setCurrentPage={setCurrentPage}
           totalPages={totalPages}
-        /> */}
+        />
       </div>
     </div>
   )
