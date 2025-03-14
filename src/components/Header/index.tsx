a9413a4c16d243a4708932d0b7c730790756867b
김보미 <kimbomi2172@naver.com>
2025-03-14 21:36:53 +0900

    ✨ feat: 마이페이지 헤더 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,28 +1,40 @@
 import Image from 'next/image'
 import logo from '@/assets/imgs/logo.png'
 import Link from 'next/link'
+import profile from '@/assets/icons/profile.png'
 
 export default function Header() {
   return (
     <header className="bg-custom-black border-custom-gray-300 fixed top-0 left-0 z-50 w-full border-b">
-      <div className="mx-auto flex w-full max-w-[1200px] items-center justify-start gap-10 px-6 text-white">
-        <Link href="/home">
-          <Image src={logo} width={200} height={50} alt="로고" />
+      <div className="mx-auto flex w-full max-w-[1200px] items-center justify-between gap-4 px-6 text-white md:gap-10">
+        <div className="flex items-center justify-start gap-4 md:gap-10">
+          <Link href="/home">
+            <Image src={logo} width={200} height={50} alt="로고" />
+          </Link>
+          <nav className="flex gap-4 md:gap-10">
+            <Link
+              href="/home"
+              className="hover:text-primary text-lg font-bold transition-all"
+            >
+              Home
+            </Link>
+            <Link
+              href="/study"
+              className="hover:text-primary text-lg font-bold transition-all"
+            >
+              Study
+            </Link>
+          </nav>
+        </div>
+        <Link href="/my">
+          <Image
+            src={profile}
+            alt="마이페이지 로고"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
         </Link>
-        <nav className="flex gap-10">
-          <Link
-            href="/home"
-            className="hover:text-primary text-lg font-bold transition-all"
-          >
-            Home
-          </Link>
-          <Link
-            href="/study"
-            className="hover:text-primary text-lg font-bold transition-all"
-          >
-            Study
-          </Link>
-        </nav>
       </div>
     </header>
   )
